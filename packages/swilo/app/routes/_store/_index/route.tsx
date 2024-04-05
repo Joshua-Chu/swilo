e351e5ae1e48d23ced2b3588dcb4cbaa62302553
Joshua-Chu <jpchu@up.edu.ph>
2024-04-06 00:27:02 +0800

    test docs should not build

--- packages/swilo/app/routes/_store/_index/route.tsx
+++ packages/swilo/app/routes/_store/_index/route.tsx
@@ -1,3 +1,3 @@
 export default function StorefrontHome() {
-  return <div className="text-3xl text-red-600">Hello Storefronts</div>;
+  return <div className="text-3xl text-red-600">Hello Storefrontss</div>;
 }
