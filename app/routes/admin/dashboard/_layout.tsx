90b68d8be537795f0421b69dfd76b6dea4ce85f4
Joshua-Chu <jpchu@up.edu.ph>
2024-04-25 14:28:24 +0800

    style: add partial admin dashboard styles

--- app/routes/admin/dashboard/_layout.tsx
+++ app/routes/admin/dashboard/_layout.tsx
@@ -1,14 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { Outlet } from "@remix-run/react";
+import { Header } from "./layout/header";
+import { SideBar } from "./layout/side-bar";
 
-export default function AdminDashboard() {
+export default function Dashboard() {
   return (
-    <div>
-      <header>
-        <Button>Logout</Button>
-      </header>
-      Hello Admin Dashboard Layout
-      <Outlet />
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <SideBar />
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <Header />
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8"></main>
+      </div>
     </div>
   );
 }
